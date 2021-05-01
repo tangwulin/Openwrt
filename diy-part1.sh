8438cc93a73693f41aeefd26c2a1ae031a845fc2
tangwulin <45118263+tangwulin@users.noreply.github.com>
2021-05-01 12:41:16 +0800

    First change
    初始设置

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/V2RaySSR/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/V2RaySSR/small' feeds.conf.default
